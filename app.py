302c0eaa35076f015de5c91aac346673b6e1d1de
George Wukoson <george@clearlayer.io>
2023-01-08 17:18:57 -0500

    Revised commit

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ import os
 import openai
 import datetime
 from docx import Document
-from flask import Flask, redirect, render_template, request, url_for, send_file
+from flask import Flask, redirect, render_template, request, url_for, send_file, request
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    user = {'username': 'User1'}
     if request.method == "POST":
         subject = request.form["subject"]
         jurisdiction = request.form["jurisdiction"]
@@ -32,7 +43,7 @@ def index():
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     docfile.save("download/string_to_word_doc.docx")
-    return render_template("index.html", result=result)
+    return render_template("index.html", user=user, result=result)
 
 def generate_prompt(doctype, subject, jurisdiction, party1, party2):
     if doctype == "memo":
